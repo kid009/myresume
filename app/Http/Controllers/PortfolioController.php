333b503ed1646fe083c97cce1151269c895bd867
kid009 <pongpoom1985@gmail.com>
2026-04-07 21:41:29 +0700

    create 2 language

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,8 +9,6 @@ class PortfolioController extends Controller
 {
     public function index()
     {
-        App::setlocale('th');
-
         $homeData = HomeDto::make();
 
         return view('portfolio.index', [
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', [PortfolioController::class, 'index'])->name('home');
 // ใส่ไว้ใน routes/web.php
 Route::get('lang/{locale}', function ($locale) {
     // เช็กว่าภาษาที่ส่งมา มีในระบบเราหรือไม่ (ป้องกันคนพิมพ์ URL มั่ว)
-    if (in_array($locale, ['en', 'th'])) {
+    if (in_array($locale, ['th', 'en'])) {
         Session::put('locale', $locale);
     }
     return redirect()->back();
